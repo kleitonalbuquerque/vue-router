f8702d10adb7aeebd2c9ff3366d8d4f964bd003b
kleitonalbuquerque <kleiton2102@gmail.com>
2022-02-28 22:49:08 -0300

    Componentes em rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,33 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Cadastro from "../views/Cadastro.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
-  }
-]
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/mariana",
+    name: "Mariana",
+    component: Cadastro,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Cadastro.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="cadastro">
+    <input type="text" name="nome" id="nome" placeholder="Nome" /><br />
+    <input type="text" name="email" id="email" placeholder="E-mail" /><br />
+    <button>Enviar</button>
+  </div>
+</template>
